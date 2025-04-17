e0c1e875ecf556db047dd390620d24bffaf3a73b
SHASHI GOWDA <itsme.shashik@gmail.com>
2025-04-17 23:45:56 +0200

    Step1 of Cmake

---
+++ SquareRoot/CMakeFiles/SquareRoot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SquareRoot.dir/SquareRoot.cpp.o"
+  "CMakeFiles/SquareRoot.dir/SquareRoot.cpp.o.d"
+  "SquareRoot"
+  "SquareRoot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SquareRoot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- SquareRoot/SquareRoot.cpp
+++ SquareRoot/SquareRoot.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <cmath>
 
+#include "SquareRootConfig.h"
+
 int main(int argc, char* argv[])
 {
-	double const inputValue = std::stod(argv[0]);	
+	std::cout << argv[0] << " Version " << SquareRoot_VERSION_MAJOR << "."
+	<< SquareRoot_VERSION_MINOR << std::endl;
+	
+	double const inputValue = std::stod(argv[1]);	
 	double outputValue = sqrt(inputValue);
-	std::cout << "The square root of "<< inputValue << "is " << outValue << std::endl;
+	std::cout << "The square root of "<< inputValue << "is " << outputValue << std::endl;
 	return 0;
 }
